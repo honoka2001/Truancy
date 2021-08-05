cbb65c8949c24eed0720604bb48b83b8ffa09308
Yoshito-4410 <ttymah@icloud.com>
2021-08-05 16:35:06 +0900

    add : DefinitionsController に index new create アクションを作成

--- api/app/controllers/definitions_controller.rb
+++ api/app/controllers/definitions_controller.rb
@@ -1,2 +1,20 @@
 class DefinitionsController < ApplicationController
+
+  def index
+    @definition = Definition.all
+  end
+
+  def new
+    @definition = Definition.new
+  end
+
+  def create
+    @definition = Definition.new
+    if @definition.save
+      render json: @definition
+    else
+      render json: @definition.errors, status: 422
+    end
+  end
+
 end
