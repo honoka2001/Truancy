348a545adeeb277421631e73c495635b14e43f6e
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2021-08-07 17:54:47 +0900

    fix：{}削除

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
-	def create
-		if user = User.find_by(user_create_params)
-			render json:{user}
-		else
-			user = User.create(user_create_params)
-			render json:{user}
-		end
-	end
-	private
+  def create
+    if user = User.find_by(user_create_params) # (uid:uid)
+    else
+      user = User.create(user_create_params)
+    end
+    render json: user
+  end
+
+  private
 
   def user_create_params
     params.require(:user).permit(:uid)
