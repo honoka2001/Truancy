e67845127ca0cd16b26984caff828527aebfc111
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2021-08-07 17:55:23 +0900

    fix：stateの利用を削除

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,50 +4,49 @@ import { RepositoryFactory } from "../repositories/RepositoryFactory";
 const userRepository = RepositoryFactory.get("users");
 
 function App() {
-  const [userData, setUserData] = useState([]);
+    const [userData, setUserData] = useState([]);
 
-  async function userPost() {
-    try {
-      const res = await userRepository.post({
-        user: {
-          uid: user.uid,
-        },
-      });
-      console.log(res);
-    } catch (error) {
-      console.log(error);
+    async function userPost(uid) {
+        try {
+            const res = await userRepository.post({
+                user: {
+                    uid: uid,
+                },
+            });
+            console.log(res);
+        } catch (error) {
+            console.log(error);
+        }
     }
-  }
 
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) => {
-      console.log(user);
-      setUserData(user);
-      userPost();
-    });
-  }, []);
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((user) => {
+            console.log(user);
+            setUserData(user);
+            userPost(user.uid);
+        });
+    }, []);
 
-  async function login() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithRedirect(provider);
-    userPost();
-  }
+    async function login() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithRedirect(provider);
+    }
 
-  function logout() {
-    firebase.auth().signOut();
-  }
+    function logout() {
+        firebase.auth().signOut();
+    }
 
-  return (
-    <div className="App">
-      <p className="App-intro">UID: {userData && userData.uid}</p>
+    return (
+        <div className="App">
+            <p className="App-intro">UID: {userData && userData.uid}</p>
 
-      {userData ? (
-        <button onClick={logout}>Google Logout</button>
-      ) : (
-        <button onClick={login}>Google Login</button>
-      )}
-    </div>
-  );
+            {userData ? (
+                <button onClick={logout}>Google Logout</button>
+            ) : (
+                <button onClick={login}>Google Login</button>
+            )}
+        </div>
+    );
 }
 
 export default App;
