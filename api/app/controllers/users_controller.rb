9a211cfc6ab6be7f5ebc2c0cc34b8e23c63bbaa5
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2021-08-07 15:33:14 +0900

    add：ユーザ登録

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
 	def create
-		
-
+		if user = User.find_by(user_create_params)
+			render json:{user}
+		else
+			user = User.create(user_create_params)
+			render json:{user}
+		end
 	end
 	private
 
