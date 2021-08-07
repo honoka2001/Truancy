d6eb3f8af3b8be36a44fe72ea7a47b0dab824d53
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2021-08-07 14:56:50 +0900

    change：yarn.lock schema

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_04_045208) do
+ActiveRecord::Schema.define(version: 2021_08_07_053350) do
 
   create_table "colors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2021_08_04_045208) do
     t.integer "target"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
   end
 
   add_foreign_key "commits", "definitions"
