87e77a9fba947fcfca1cd416edfe6b207457442f
HayashiMitsu <hm20011027@gmail.com>
2021-08-09 02:02:36 +0900

    コンフリクト解消

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def current_user(uid)
+    current_user = User.find_by(uid: uid)
+  end
 end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_04_045208) do
+ActiveRecord::Schema.define(version: 2021_08_07_094111) do
 
   create_table "colors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_08_04_045208) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
     t.index ["definition_id"], name: "index_commits_on_definition_id"
     t.index ["user_id"], name: "index_commits_on_user_id"
   end
@@ -53,6 +54,7 @@ ActiveRecord::Schema.define(version: 2021_08_04_045208) do
     t.integer "target"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
   end
 
   add_foreign_key "commits", "definitions"
