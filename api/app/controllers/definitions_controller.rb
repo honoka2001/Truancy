58bf36a201333f073c76b961e153a3eb29ca4e0b
Yoshito-4410 <ttymah@icloud.com>
2021-08-10 00:08:36 +0900

    add : definition controllerを作成

--- api/app/controllers/definitions_controller.rb
+++ api/app/controllers/definitions_controller.rb
@@ -1,12 +1,15 @@
 class DefinitionsController < ApplicationController
-
   def index
-    definitions = Definition.all
+    # userのインスタンス)(レコード(行))を取得
+    # user = User.find_by(uid: uid)
+    user = User.find_by(definition_index_params)
+    # userが複数持ってる(has_many)Definition
+    definitions = user.definitions # 複数形はs
     render json: definitions
   end
 
   def create
-    definition = Definition.new
+    definition = Difinition.new(definitions_create_params)
     if definition.save
       render json: definition
     else
@@ -14,4 +17,16 @@ class DefinitionsController < ApplicationController
     end
   end
 
+  private
+
+  # index用
+  def definition_index_params
+    params.require(:user).permit(:uid)
+  end
+
+  # create用
+  def definitions_create_params
+    params.require(:definition).permit(:name, :detail, :color_id, :user_id)
+  end
+  # uid = フロントから渡されたuid
 end
