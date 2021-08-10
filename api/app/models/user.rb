3a4fa7dc3889f2d5bed015a5fe1dd43660bbba85
honoka2001 <honoka519pk@gmail.com>
2021-08-10 16:39:17 +0900

    add : モチベーション関連のアソシエーション

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :commits, dependent: :destroy
   has_many :definitions, dependent: :destroy
+  has_many :motivations
 
   has_many :follower, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
   has_many :followed, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
