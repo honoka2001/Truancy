a1be790f1bbef73e4521435072b75e49fc9ff7ca
honoka2001 <honoka519pk@gmail.com>
2021-08-13 01:32:13 +0900

    add : 傾き算出メソッド

--- api/app/models/motivation.rb
+++ api/app/models/motivation.rb
@@ -8,4 +8,16 @@ class Motivation < ApplicationRecord
         sub = Motivation.find_by(user_id: user_id, date: sub_date).daily_total_commits - user.target
         sub_param = sub * sub_weight
       end
+
+      # 傾きの算出
+      def get_diff_param
+        diff_weight = 5
+        diff_start_date = date.ago(1.days)
+        diff_end_date = date.ago(2.days)
+
+        diff = Motivation.find_by(user_id: user_id,
+                                  date: diff_start_date).daily_total_commits / Motivation.find_by(user_id: user_id,
+                                                                                                  date: diff_end_date).daily_total_commits.to_f
+        diff_param = diff * diff_weight
+      end
 end
