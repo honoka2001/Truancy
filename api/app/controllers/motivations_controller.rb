a0118668fe0d647f85a0922bffe14b5e296819b5
honoka2001 <honoka519pk@gmail.com>
2021-08-13 01:39:20 +0900

    add : motivation controller モチベーション算出機能

--- api/app/controllers/motivations_controller.rb
+++ api/app/controllers/motivations_controller.rb
@@ -1,3 +1,10 @@
 class MotivationsController < ApplicationController
+  def index
+    user = User.find(1)
+    date = Date.today
 
+    motivations = user.motivations
+    motivation = motivations.find_by(date: date)
+    motivation.get_motivation
+  end
 end
