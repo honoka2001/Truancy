4cd277055a090d6d81f7c6a7361bfb3636f8bf75
honoka2001 <honoka519pk@gmail.com>
2021-08-13 01:30:48 +0900

    add : 偏差算出メソッド

--- api/app/models/motivation.rb
+++ api/app/models/motivation.rb
@@ -1,4 +1,11 @@
 class Motivation < ApplicationRecord
   belongs_to :user
   validates :date, uniqueness: { scope: :user_id }
+      # 偏差の算出
+      def get_sub_param
+        sub_weight = 1.5
+        sub_date = date.ago(1.days)
+        sub = Motivation.find_by(user_id: user_id, date: sub_date).daily_total_commits - user.target
+        sub_param = sub * sub_weight
+      end
 end
