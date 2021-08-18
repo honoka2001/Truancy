df82902017fe1a2b1105b9cdede322204c271233
Yoshito-4410 <ttymah@icloud.com>
2021-08-18 16:57:14 +0900

    add : firebase から uid を取得。 user table からユーザーデータを取得。

--- app/pages/TestDefinitionInput.js
+++ app/pages/TestDefinitionInput.js
@@ -1,9 +1,40 @@
+import React, {useState, useEffect} from "react";
 import DefinitionIndex from "../components/definitions"
+import firebase from "../components/firebase";
+import { RepositoryFactory } from "../repositories/RepositoryFactory";
+const userRepository = RepositoryFactory.get("users");
+/*
+firebase.auth().onAuthStateChanged((user) => {
+        
+        });
+*/
 
 export default function Home() {
+  const [userData, setUserData] = useState([])
+  async function userPost() {
+    try {
+        const res = await userRepository.post({
+            user: {
+              uid: userData.uid
+            },
+        });
+        console.log(res.data.id);
+    } catch (error) {
+        console.log(err);
+    }
+  }
+  // 更新もしくは表示された(マウント・アンマウント)直後にuserData中身をコンソールで呼び出す
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+        //user.uid => ⭐️ログイン中のユーザのuid
+        //user.displayName => ログイン中のユーザーの名前
+      setUserData(user)
+    });
+  }, [])
+
   return (
     <div>
-      <DefinitionIndex/>
+      <DefinitionIndex userData={userData} />
     </div>
   )
 }
