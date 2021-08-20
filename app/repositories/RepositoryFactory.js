2653e471a1d20931bd616388d127333dea2b8b0f
honoka2001 <honoka519pk@gmail.com>
2021-08-20 12:26:44 +0900

    add : motivationRepository showGet

--- app/repositories/RepositoryFactory.js
+++ app/repositories/RepositoryFactory.js
@@ -1,9 +1,11 @@
 import UserRepository from "./userRepository";
 import CommitRepository from "./commitRepository";
+import MotivationRepository from "./motivationRepository";
 
 const repositories = {
     users: UserRepository,
     commits: CommitRepository,
+    motivations: MotivationRepository,
 };
 
 export const RepositoryFactory = {
