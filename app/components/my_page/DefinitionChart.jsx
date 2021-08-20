8eb8825f781d5658edfe54c56c179a91c672868d
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2021-08-20 16:32:14 +0900

    disable：不要部

--- app/components/my_page/DefinitionChart.jsx
+++ app/components/my_page/DefinitionChart.jsx
@@ -4,33 +4,11 @@ import Chart from "react-apexcharts";
 function DefinitionChart() {
     const [options, setOptions] = useState({
         chart: {
-            width: 380,
             type: "pie",
         },
         labels: ["数学", "国語", "理科"],
-        responsive: [
-            {
-                breakpoint: 480,
-                options: {
-                    chart: {
-                        width: 200,
-                    },
-                    legend: {
-                        position: "bottom",
-                    },
-                },
-            },
-        ],
     });
     const [series, setSeries] = useState([45, 52, 38]);
-    return (
-        <div className="app">
-            <div className="row">
-                <div className="mixed-chart">
-                    <Chart options={options} series={series} type="pie" width="500" />
-                </div>
-            </div>
-        </div>
-    );
+    return <Chart options={options} series={series} type="pie" width="100%" />;
 }
 export default DefinitionChart;
