1ebb6eb97f7fb225ee5ed72e546e4354393d0c44
honoka2001 <honoka519pk@gmail.com>
2021-08-20 18:30:30 +0900

    add : dairy_total_commit算出メソッド

--- api/app/models/commit.rb
+++ api/app/models/commit.rb
@@ -3,4 +3,11 @@
 class Commit < ApplicationRecord
   belongs_to :user
   belongs_to :definition
+
+  def add_daily_total_commits
+    motivation = Motivation.find_by(user_id: self.user_id, date: self.date)
+    daily_total_commits = self.count + motivation.daily_total_commits
+    motivation.update(daily_total_commits: daily_total_commits)
+  end
+
 end
