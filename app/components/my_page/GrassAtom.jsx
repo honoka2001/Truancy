1b90a79a951aabadf8fca330e09a59b50d8f63f0
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2021-08-22 17:03:21 +0900

    add：草の最小単位コンポーネント

---
+++ app/components/my_page/GrassAtom.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import styles from "../../styles/my_page/GrassAtom.module.css";
+
+function GrassAtom() {
+    return (
+        <div className={styles.maru_area}>
+            <div className={styles.maru}></div>
+        </div>
+    );
+}
+export default GrassAtom;
