bc8ccb18e7fabe7871ebabd6f320712aaaef0f23
HayashiMitsu <hm20011027@gmail.com>
2021-08-23 01:08:50 +0900

    add:色を表示するよう変更

--- app/components/definitions/index.jsx
+++ app/components/definitions/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 function DefinitionIndex(props) {
   const [definitions, setDefinitions] = useState([]);
@@ -19,21 +19,21 @@ function DefinitionIndex(props) {
     } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   useEffect(() => {
     definitionPost(props.userId);
-  }, [props.userId])
+  }, [props.userId]);
 
   const list = definitions.map((data) => (
-    <li key={data.id}>{data.definitions_name} {data.detail} {data.colors_name}</li>
+    <div style={{ display: "flex" }} key={data.id}>
+      {data.definitions_name} {data.detail}{" "}
+      <div style={{ width: 20, height: 20, backgroundColor: data.code }}></div>{" "}
+      {data.colors_name}
+    </div>
   ));
 
-  return (
-    <div>
-      {list}
-    </div>
-  )
+  return <div>{list}</div>;
 }
 
 export default DefinitionIndex;
