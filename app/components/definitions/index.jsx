45b82fa894d685bcac88fadb9660d68924f9bffb
HayashiMitsu <hm20011027@gmail.com>
2021-08-23 00:24:11 +0900

    add:difinition表示コンポーネント作成

--- app/components/definitions/index.jsx
+++ app/components/definitions/index.jsx
@@ -1,10 +1,37 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
-function DefinitionIndex() {
+function DefinitionIndex(props) {
+  const [definitions, setDefinitions] = useState([]);
+
+  const definitionPost = (userId) => {
+    try {
+      axios
+        .get("http://localhost:3000/definitions", {
+          params: {
+            id: userId,
+          },
+        })
+        .then(function (res) {
+          console.log(res.data);
+          setDefinitions(res.data);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    definitionPost(props.userId);
+  }, [props.userId])
+
+  const list = definitions.map((data) => (
+    <li key={data.id}>{data.definitions_name} {data.detail} {data.colors_name}</li>
+  ));
+
   return (
     <div>
-      DefinitionIndex
+      {list}
     </div>
   )
 }
