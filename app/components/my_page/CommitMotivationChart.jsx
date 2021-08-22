f7d26fd58dbde53a2039e386b85e483104a18d34
honoka2001 <honoka519pk@gmail.com>
2021-08-23 02:11:25 +0900

    add : 定義別コミット数のグラフの本データ表示

--- app/components/my_page/CommitMotivationChart.jsx
+++ app/components/my_page/CommitMotivationChart.jsx
@@ -1,39 +1,65 @@
 import React, { useState, useEffect } from "react";
 import Chart from "react-apexcharts";
 
-function CommitMotivationChart() {
-    const [options, setOptions] = useState({
-        chart: {
-            id: "basic-bar",
-            width: "50%",
+function CommitMotivationChart(props) {
+  const [options, setOptions] = useState({
+    chart: {
+      id: "basic-bar",
+      width: "50%",
+    },
+    xaxis: {
+      //   categories: props.week_date,
+      categories: [1992, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+    },
+    yaxis: [
+      {
+        title: {
+          text: "モチベーション率(%)",
         },
-        xaxis: {
-            categories: [1992, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+      },
+      {
+        opposite: true,
+        title: {
+          text: "コミット数(commits)",
         },
-        yaxis: [
-            {
-                title: {
-                    text: "Series A",
-                },
-            },
-            {
-                opposite: true,
-                title: {
-                    text: "Series B",
-                },
-            },
-        ],
+      },
+    ],
+  });
+  const [series, setSeries] = useState([
+    {
+      name: "モチベーション率",
+      //   data: props.week_motivation_per,
+      data: [30, 40, 45, 50, 49, 60, 70, 91],
+    },
+    {
+      name: "コミット数",
+      //   data: props.week_daily_total_commits,
+      data: [20, 30, 35, 40, 39, 50, 60, 81],
+    },
+  ]);
+  useEffect(() => {
+    setOptions({
+      xaxis: {
+        categories: props.week_date,
+      },
     });
-    const [series, setSeries] = useState([
-        {
-            name: "series-1",
-            data: [30, 40, 45, 50, 49, 60, 70, 91],
-        },
-        {
-            name: "series-2",
-            data: [20, 30, 35, 40, 39, 50, 60, 81],
-        },
+
+    setSeries([
+      {
+        name: "モチベーション率",
+        data: props.week_motivation_per,
+      },
+      {
+        name: "コミット数",
+        data: props.week_daily_total_commits,
+      },
     ]);
-    return <Chart options={options} series={series} type="line" height="100%" />;
+  }, [props.week_date]);
+
+  return (
+    <div>
+      <Chart options={options} series={series} type="line" height="100%" />;
+    </div>
+  );
 }
 export default CommitMotivationChart;
