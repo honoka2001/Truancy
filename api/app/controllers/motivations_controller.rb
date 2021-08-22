f8101769eda7e19cc3da810f25ab410cb09ad4e0
honoka2001 <honoka519pk@gmail.com>
2021-08-23 00:47:51 +0900

    add : Motivationモデルにレコードのない日にコミット数0を挿入

--- api/app/controllers/motivations_controller.rb
+++ api/app/controllers/motivations_controller.rb
@@ -3,10 +3,21 @@
 class MotivationsController < ApplicationController
   def show
     user = User.find(params[:id])
-    date = Time.current
+    today = Date.today
 
-    motivations = user.motivations.order(date: "DESC")
-    motivation = motivations.find_by(date: date)
+    motivations = user.motivations.order(date: 'DESC')
+
+    # motivationsテーブルにレコードのない日にコミット数0を挿入しモチベーション算出
+    non_commits_date = motivations.last.date + 1
+    (non_commits_date..today).each do |date|
+      next if motivations.find_by(date: date)
+
+      Motivation.create(date: date, user_id: user.id, daily_total_commits: 0)
+      motivation = Motivation.find_by(date: date, user_id: user.id)
+      motivation.get_motivation
+    end
+    # 今日のモチベーション算出
+    motivation = motivations.find_by(date: today)
     motivation.get_motivation
 
     # 今日のモチベーション率
