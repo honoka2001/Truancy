405ebdf5e28a5c66d6047a6a5d06c44e5a8ffc8c
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2021-08-23 16:13:43 +0900

    add：カレンダー

--- app/components/my_page/GrassAtom.jsx
+++ app/components/my_page/GrassAtom.jsx
@@ -1,11 +1,28 @@
 import React, { useState, useEffect } from "react";
 import styles from "../../styles/my_page/GrassAtom.module.css";
 
-function GrassAtom() {
-    return (
-        <div className={styles.maru_area}>
-            <div className={styles.maru}></div>
-        </div>
-    );
+function GrassAtom(props) {
+    function kusaType(commits, motivation) {
+        // 変数名=>コミット数の多さ_モチベーションの高さ
+        let many_high = motivation >= 50 && commits >= 10;
+        let many_low = motivation < 50 && commits >= 10;
+        let few_high = motivation >= 50 && commits < 10;
+        let few_low = motivation < 50 && commits < 10;
+        let empty = commits == 0;
+
+        if (empty) return <div className={style.empty_commit}></div>;
+        else if (many_high)
+            return <div className={`${styles.many_commits} ${styles.high_motivation}`}></div>;
+        else if (many_low)
+            return <div className={`${styles.many_commits} ${styles.low_motivation}`}></div>;
+        else if (few_high)
+            return <div className={`${styles.few_commits} ${styles.high_motivation}`}></div>;
+        else if (few_low)
+            return <div className={`${styles.few_commits} ${styles.low_motivation}`}></div>;
+    }
+
+   
+    return <div className={styles.maru_area}>{kusaType(props.commits, props.motivation)}</div>;
+
 }
 export default GrassAtom;
